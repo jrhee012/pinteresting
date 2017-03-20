405e5dd87874d65adc7ca261fbb028f4fb708c72
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2017-03-20 16:12:00 -0400

    names added

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ApplicationRecord
+  validates :image, presence: true
+  validates :description, presence: true
+  
   belongs_to :user
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
